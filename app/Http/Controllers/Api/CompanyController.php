65a7bb695c0963c4facd7d5db63c0dd6d4b4f7b8
Ricardo Vargas V <vargasvargas.ric@gmail.com>
2024-01-15 11:49:02 -0500

    Desarrollando login

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -10,55 +10,59 @@ use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    function index(Request $request)
-    {
-        $items = Company::where('name', 'like', '%' . $request->search . '%')
-            ->orWhere('email', 'like', '%' . $request->search . '%')
-            ->orWhere('phone', 'like', '%' . $request->search . '%')
-            ->orderBy('id', 'desc');
+    function register
 
-        $items = ($request->perPage == 'all') ? $items->get() : $items->paginate($request->perPage);
+    // function index(Request $request)
+    // {
+    //     $items = Company::where('name', 'like', '%' . $request->search . '%')
+    //         ->orWhere('email', 'like', '%' . $request->search . '%')
+    //         ->orWhere('phone', 'like', '%' . $request->search . '%')
+    //         ->orderBy('id', 'desc');
 
-        return CompanyResource::collection($items);
-    }
+    //     $items = ($request->perPage == 'all') ? $items->get() : $items->paginate($request->perPage);
 
-    function store(CompanyRequest $request)
-    {
-        $item = Company::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'phone' => $request->phone,
-        ]);
+    //     return CompanyResource::collection($items);
+    // }
 
-        return CompanyResource::make($item)->additional([
-            'message' => 'Empresa Registrada.'
-        ]);
-    }
+    // function store(CompanyRequest $request)
+    // {
+    //     $item = Company::create([
+    //         'name' => $request->name,
+    //         'email' => $request->email,
+    //         'phone' => $request->phone,
+    //     ]);
 
-    function show(Company $company)
-    {
-        return CompanyResource::make($company);
-    }
+    //     return CompanyResource::make($item)->additional([
+    //         'message' => 'Empresa Registrada.'
+    //     ]);
+    // }
 
-    function update(CompanyRequest $request, Company $company)
-    {
-        $company->update([
-            'name' => $request->name,
-            'email' => $request->email,
-            'phone' => $request->phone,
-        ]);
+    // function show(Company $company)
+    // {
+    //     return CompanyResource::make($company);
+    // }
 
-        return CompanyResource::make($company)->additional([
-            'message' => 'Empresa Actualizada.'
-        ]);
-    }
+    // function update(CompanyRequest $request, Company $company)
+    // {
+    //     $company->update([
+    //         'name' => $request->name,
+    //         'email' => $request->email,
+    //         'phone' => $request->phone,
+    //     ]);
 
-    function destroy(Company $company)
-    {
-        $company->delete();
+    //     return CompanyResource::make($company)->additional([
+    //         'message' => 'Empresa Actualizada.'
+    //     ]);
+    // }
 
-        return CompanyResource::make($company)->additional([
-            'message' => 'Empresa Eliminada.'
-        ]);
-    }
+    // function destroy(Company $company)
+    // {
+    //     $company->delete();
+
+    //     return CompanyResource::make($company)->additional([
+    //         'message' => 'Empresa Eliminada.'
+    //     ]);
+    // }
+
+
 }
