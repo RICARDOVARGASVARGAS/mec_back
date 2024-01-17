8adc4b2e94bcb6421127a0c2982d6aa23538554b
Ricardo Vargas Vargas <vargasvargas.ric@gmail.com>
2024-01-17 11:12:59 -0500

    Modulo Usuario

--- routes/user.php
+++ routes/user.php
@@ -8,6 +8,6 @@ Route::post('registerUser', [UserController::class, 'registerUser'])->name('regi
 Route::get('getUser/{user}', [UserController::class, 'getUser'])->name('getUser');
 Route::put('updateUser/{user}', [UserController::class, 'updateUser'])->name('updateUser');
 Route::delete('deleteUser/{user}', [UserController::class, 'deleteUser'])->name('deleteUser');
-Route::post('users/{user}/resetPassword', [UserController::class, 'resetPassword'])->name('users.resetPassword');
-Route::post('users/{user}/changePassword', [UserController::class, 'changePassword'])->name('users.changePassword');
-Route::post('users/updatePermission', [UserController::class, 'updatePermission'])->name('users.updatePermission');
+Route::post('resetPassword/{user}', [UserController::class, 'resetPassword'])->name('users.resetPassword');
+Route::post('changePassword/{user}', [UserController::class, 'changePassword'])->name('users.changePassword');
+Route::post('updatePermission', [UserController::class, 'updatePermission'])->name('users.updatePermission');
