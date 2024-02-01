517ee100b0c222ce0fb8c1b863cc49040796051a
Ricardo Vargas V <vargasvargas.ric@gmail.com>
2024-02-01 14:38:26 -0500

    Desplegando

--- routes/calculate.php
+++ routes/calculate.php
@@ -3,16 +3,16 @@
 use App\Http\Controllers\Api\CalculateController;
 use Illuminate\Support\Facades\Route;
 
-// Route::group(['middleware' => 'auth:api', 'throttle:500,1'], function () {
-Route::post('getCalculates', [CalculateController::class, 'getCalculates'])->name('getCalculates');
-Route::post('getCalculate/{calculate}', [CalculateController::class, 'getCalculate'])->name('getCalculate');
-Route::post('registerCalculate', [CalculateController::class, 'registerCalculate'])->name('registerCalculate');
-Route::post('updateCalculate/{calculate}', [CalculateController::class, 'updateCalculate'])->name('updateCalculate');
-Route::delete('deleteCalculate/{calculate}', [CalculateController::class, 'deleteCalculate'])->name('deleteCalculate');
+Route::group(['middleware' => 'auth:api', 'throttle:500,1'], function () {
+    Route::post('getCalculates', [CalculateController::class, 'getCalculates'])->name('getCalculates');
+    Route::post('getCalculate/{calculate}', [CalculateController::class, 'getCalculate'])->name('getCalculate');
+    Route::post('registerCalculate', [CalculateController::class, 'registerCalculate'])->name('registerCalculate');
+    Route::post('updateCalculate/{calculate}', [CalculateController::class, 'updateCalculate'])->name('updateCalculate');
+    Route::delete('deleteCalculate/{calculate}', [CalculateController::class, 'deleteCalculate'])->name('deleteCalculate');
 
-Route::get('getCalculateDetail/{calculate}', [CalculateController::class, 'getCalculateDetail'])->name('getCalculateDetail');
-Route::post('addProduct', [CalculateController::class, 'addProduct'])->name('addProduct');
-Route::post('removeProduct', [CalculateController::class, 'removeProduct'])->name('removeProduct');
-Route::post('addService', [CalculateController::class, 'addService'])->name('addService');
-Route::post('removeService', [CalculateController::class, 'removeService'])->name('removeService');
-// });
+    Route::get('getCalculateDetail/{calculate}', [CalculateController::class, 'getCalculateDetail'])->name('getCalculateDetail');
+    Route::post('addProduct', [CalculateController::class, 'addProduct'])->name('addProduct');
+    Route::post('removeProduct', [CalculateController::class, 'removeProduct'])->name('removeProduct');
+    Route::post('addService', [CalculateController::class, 'addService'])->name('addService');
+    Route::post('removeService', [CalculateController::class, 'removeService'])->name('removeService');
+});
--- routes/sale.php
+++ routes/sale.php
@@ -3,20 +3,20 @@
 use App\Http\Controllers\Api\SaleController;
 use Illuminate\Support\Facades\Route;
 
-// Route::group(['middleware' => 'auth:api', 'throttle:500,1'], function () {
-Route::post('getSales', [SaleController::class, 'getSales'])->name('getSales');
-Route::post('getSale/{sale}', [SaleController::class, 'getSale'])->name('getSale');
-Route::post('registerSale', [SaleController::class, 'registerSale'])->name('registerSale');
-Route::post('updateSale/{sale}', [SaleController::class, 'updateSale'])->name('updateSale');
-Route::delete('deleteSale/{sale}', [SaleController::class, 'deleteSale'])->name('deleteSale');
+Route::group(['middleware' => 'auth:api', 'throttle:500,1'], function () {
+    Route::post('getSales', [SaleController::class, 'getSales'])->name('getSales');
+    Route::post('getSale/{sale}', [SaleController::class, 'getSale'])->name('getSale');
+    Route::post('registerSale', [SaleController::class, 'registerSale'])->name('registerSale');
+    Route::post('updateSale/{sale}', [SaleController::class, 'updateSale'])->name('updateSale');
+    Route::delete('deleteSale/{sale}', [SaleController::class, 'deleteSale'])->name('deleteSale');
 
-Route::get('getSaleDetail/{sale}', [SaleController::class, 'getSaleDetail'])->name('getSaleDetail');
-Route::post('addProduct', [SaleController::class, 'addProduct'])->name('addProduct');
-Route::post('removeProduct', [SaleController::class, 'removeProduct'])->name('removeProduct');
-Route::post('addService', [SaleController::class, 'addService'])->name('addService');
-Route::post('removeService', [SaleController::class, 'removeService'])->name('removeService');
-Route::post('addPayment', [SaleController::class, 'addPayment'])->name('addPayment');
-Route::post('removePayment', [SaleController::class, 'removePayment'])->name('removePayment');
+    Route::get('getSaleDetail/{sale}', [SaleController::class, 'getSaleDetail'])->name('getSaleDetail');
+    Route::post('addProduct', [SaleController::class, 'addProduct'])->name('addProduct');
+    Route::post('removeProduct', [SaleController::class, 'removeProduct'])->name('removeProduct');
+    Route::post('addService', [SaleController::class, 'addService'])->name('addService');
+    Route::post('removeService', [SaleController::class, 'removeService'])->name('removeService');
+    Route::post('addPayment', [SaleController::class, 'addPayment'])->name('addPayment');
+    Route::post('removePayment', [SaleController::class, 'removePayment'])->name('removePayment');
 
-Route::post('getProfit', [SaleController::class, 'getProfit'])->name('getProfit');
-// });
+    Route::post('getProfit', [SaleController::class, 'getProfit'])->name('getProfit');
+});
